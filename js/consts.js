6e0babb0b40603e84250adb805ee727af19426ea
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-27 14:11:49 -0800

    Update consts.js

--- js/consts.js
+++ js/consts.js
@@ -1,3 +1,3 @@
-export const debug = location.hostname.endsWith('.netlify.live');
+export const debug = ! (location.hostname.endsWith('.kernvalley.us') || location.hostname === 'kernvalley.us');
 export const domain = debug ? location.hostname : 'kernvalley.us';
 export const GA = null;
